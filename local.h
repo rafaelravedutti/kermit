f111b3c2a828f30129a9d47a15cf1642bffa9c89
Rafael Ravedutti Lucio Machado <rafaelraveduttilm@hotmail.com>
2015-05-01 11:29:17 -0300

    Add headers from init_directory and get_current_directory functions
    Signed-off-by: Rafael Ravedutti Lucio Machado <rafaelraveduttilm@hotmail.com>

--- local.h
+++ local.h
@@ -1,3 +1,5 @@
+void init_directory();
+char *get_current_directory();
 int change_directory(const char *path, int verbose);
 char *get_current_directory_list(int all, int list, unsigned int *length);
 FILE *fopen_current_dir(const char *path, const char *mode);
